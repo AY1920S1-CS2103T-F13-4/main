b8c689ab6ce0e3709de87891e10793fdef094d5d
CK <wongchuankai97@gmail.com>
2019-10-04 00:31:26 +0800

    a

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -1,20 +1,14 @@
 package seedu.address.model.person;
 
-
 import static java.util.Objects.requireNonNull;
 
 /**
  * Represents a Person's remark in the address book.
- * Guarantees: immutable; is always valid.
+ * Guarantees: immutable; is always valid
  */
 public class Remark {
     public final String value;
 
-    /**
-     * Constructs an {@code Remark}.
-     *
-     * @param remark A valid remark.
-     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
@@ -28,13 +22,12 @@ public class Remark {
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
-                || (other instanceof Address // instanceof handles nulls
-                && value.equals(((Address) other).value)); // state check
+                || (other instanceof Remark // instanceof handles nulls
+                && value.equals(((Remark) other).value)); // state check
     }
 
     @Override
     public int hashCode() {
         return value.hashCode();
     }
-
 }
